343d788e5e3de1c581be126cc9cbce62c9d8c121
unknown <sistemas@VRE_NB_CAP1809.inacap.cl>
2020-02-25 22:04:04 -0300

    Avance de memorice y Pokemon

--- src/js/memorice.js
+++ src/js/memorice.js
@@ -9,13 +9,28 @@ function iniciar() {
     var largo = imagenes.length
 
     for (j = 0; j < 16; j++) {
-        random = Math.floor(Math.random() * (largo - 1));
+        random = Math.floor(Math.random() * (largo));
         usados[j] = imagenes[random];
         imagenes.splice(random, 1);
         largo = imagenes.length;
     }
+
     for (var k = 0; k < 16; k++) {
         document.getElementById(k).innerHTML = '<img src="../src/img/memorice/' + usados[k] + '.jpg">';
     }
-    return usados;
 }
+
+$(document).ready(function() {
+    var idFoto;
+    var arregloFotos;
+
+    $('#iniciar').click(function() {
+        $('img').delay(1000).fadeOut();
+    });
+
+    $('td').click(function() {
+        var idFoto = $(this).attr('id');
+        document.getElementById(idFoto).innerHTML = '<img src="../src/img/memorice/' + usados[idFoto] + '.jpg">';
+    })
+
+});
